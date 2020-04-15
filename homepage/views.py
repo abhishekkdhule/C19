8123eac023135d1f3728a777dd656a13997363a4
Abhishek Dhule <52121143+abhi9137@users.noreply.github.com>
2020-04-15 14:23:27 +0530

    Update views.py

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import pandas as pd
 
+
+
 df=pd.read_json("https://covid19.mathdro.id/api/countries/India")
 # print(df.head(1))
 # print(df[['confirmed','recovered','deaths','lastUpdate']])
@@ -14,14 +16,25 @@ df=pd.read_json("https://covid19.mathdro.id/api/countries/India")
 # print("total no. of recovery = ",recovery.value)
 # print("total no. of deaths = ",death.value)
 # Create your views here.
+first=pd.read_json("https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise")
+# print(first.head())
+# print(first['data'])
+# print(first.iloc[3,1])
+# total=first.iloc[3,1]
+# state=first.iloc[2,1]
+# print(state)
+
 def homepage_view(request):
-    df=pd.read_json("https://covid19.mathdro.id/api/countries/India")
-    confirmed=df['confirmed'].head(1)
-    recovery=df['recovered'].head(1)
-    death=df['deaths'].head(1)
+    first=pd.read_json("https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise")
+    print(first.head())
+    # print(first['data'])
+    # print(first.iloc[3,1])
+    total=first.iloc[3,1]
+    state=first.iloc[2,1]
+    # print(state)
     context={
-        'confirmed':confirmed.value,
-        'recovery':recovery.value,
-        'deaths':death.value,
+        'total':total,
+        'delhi':state[1],
+        'state_data':state,
     }
     return render(request,'homepage/homepage.html',context)
